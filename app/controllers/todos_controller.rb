fd6e475ab8472f61f3ea0fcea0874da44d8969fa
axiotroph@lotus <izz@izabyl.com>
2019-04-25 00:19:51 -0700

    minimal todo list

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,26 @@
+class TodosController < ApplicationController
+  def index
+    @todos = Todo.all
+  end
+
+  def new
+  end
+  
+  def create
+    @todo = Todo.new(params.require(:todo).permit(:title))
+    @todo.save
+
+    redirect_to todos_path
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+
+    redirect_to todos_path
+  end
+end
