c49021db613971ac02b20e5c011642302cce8545
axiotroph@lotus <izz@izabyl.com>
2019-04-29 22:09:22 -0700

    autofocus new todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,14 +3,14 @@ class TodosController < ApplicationController
     @todos = Todo.all
   end
 
+  def show
+    @todo = Todo.find(params[:id])
+  end
+
   def create
     @todo = Todo.new(params.require(:todo).permit(:title))
     @todo.save
     redirect_to todos_path
-  end
-
-  def show
-    @todo = Todo.find(params[:id])
   end
 
   def destroy
